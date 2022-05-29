a09856bc0511161e16e8f29a7d934eb76d141915
TwitoCode <twitodev@gmail.com>
2022-05-29 17:25:35 -0400

    Added proper user profile

--- server/src/auth/strategies/jwt.strategy.ts
+++ server/src/auth/strategies/jwt.strategy.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@nestjs/common";
 import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
+import { UsersService } from "../../users/users.service";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-	constructor() {
+	constructor(private readonly userService: UsersService) {
 		super({
 			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
 			ignoreExpiration: false,
@@ -13,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
 	}
 
 	async validate({ id, username }: any) {
-		return { id, username };
+		return this.userService.findOne(id);
 	}
 }
